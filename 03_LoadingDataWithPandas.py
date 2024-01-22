3981a303a8406a1f41fba3f41bc7acadb8d220e0
suryawanshishreyas <suryawanshishreyas@gmail.com>
2024-01-22 15:03:56 +0530

    matplot usecase

---
+++ 03_LoadingDataWithPandas.py
@@ -0,0 +1,22 @@
+import time 
+import sys
+import numpy as np 
+
+import matplotlib.pyplot as plt
+
+
+def Plotvec2(a,b):
+    ax = plt.axes()# to generate the full window axes
+    ax.arrow(0, 0, *a, head_width=0.05, color ='r', head_length=0.1)#Add an arrow to the  a Axes with arrow head width 0.05, color red and arrow head length 0.1
+    plt.text(*(a + 0.1), 'a')
+    ax.arrow(0, 0, *b, head_width=0.05, color ='b', head_length=0.1)#Add an arrow to the  b Axes with arrow head width 0.05, color blue and arrow head length 0.1
+    plt.text(*(b + 0.1), 'b')
+    plt.ylim(-2, 2)#set the ylim to bottom(-2), top(2)
+    plt.xlim(-2, 2)#set the xlim to left(-2), right(2)
+
+a = np.array([-1,1])
+b = np.array([1,1])
+result = np.dot(a,b)
+Plotvec2(a,b)
+plt.plot(a,b)
+plt.show()
